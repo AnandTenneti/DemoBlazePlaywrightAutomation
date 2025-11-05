412aa7e01d99c27cb6377a692e17adad8a10d02c
AnnadTenneti <tenneti.anand@gmail.com>
2025-11-05 23:34:51 +0530

    Adding testdata

---
+++ testdata/testdata.js
@@ -0,0 +1,16 @@
+export const categoryData = [
+  { name: 'Phones', expectedCount: 7 },
+  { name: 'Laptops', expectedCount: 6 },
+  { name: 'Monitors', expectedCount: 2 }
+];
+
+export const loginData = [
+  { email: 'test@test.com', password: 'test' },
+  { email: 'test', password: 'test' },
+];
+
+export const productData=[
+  {name:'Samsung galaxy s6',price: '$360'},
+  {name:'Nexus 6',price: '$650'},
+  {name:'Sony vaio i5',price: '$790'},
+]
