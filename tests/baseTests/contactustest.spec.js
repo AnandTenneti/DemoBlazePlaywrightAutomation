104cad195cf1041a19196ae895b13a6f17835c90
AnnadTenneti <tenneti.anand@gmail.com>
2025-11-08 21:36:56 +0530

    Adjusting the formatting in carttest and contactustest files

--- tests/baseTests/contactustest.spec.js
+++ tests/baseTests/contactustest.spec.js
@@ -1,12 +1,13 @@
-import { test } from '../baseTests/BaseTest'
+import { test } from '../baseTests/BaseTest';
 
 test('Verify contact us ', async ({ headerPage, contactUsPage }) => {
-    let senderEmail = "test@test.com", senderName = "Anand", senderMessage = "This is a demo message"
-    await test.step('Click on Contact Us link', async () => {
-        await headerPage.clickOnContactUsLink()
-    })
-    await test.step('Fill details and send message', async () => {
-        await contactUsPage.
-            sendContactMessage(senderEmail, senderName, senderMessage)
-    })
-})
+  let senderEmail = 'test@test.com',
+    senderName = 'Anand',
+    senderMessage = 'This is a demo message';
+  await test.step('Click on Contact Us link', async () => {
+    await headerPage.clickOnContactUsLink();
+  });
+  await test.step('Fill details and send message', async () => {
+    await contactUsPage.sendContactMessage(senderEmail, senderName, senderMessage);
+  });
+});
